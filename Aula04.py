552fefa896a5ae64fad19a273662dc5575d86d52
Karine <160532475+KarineBarbosa77@users.noreply.github.com>
2024-07-16 08:52:09 -0300

    Aula04.py

---
+++ Aula04.py
@@ -0,0 +1,5 @@
+nome = input('Qual o seu nome? ') # O input é a entrada de dados, gera interação com o usuário
+idade = input('Qual a sua idade? ')
+peso = input('Qual o seu peso? ')
+
+print(nome, idade, peso)
